6ed0318c553ba1cbdfd5d70639123c48c94d3a99
Zander <zanderalastorufo@gmail.com>
2022-03-31 23:32:56 +0800

    add keyboard listener !!!

--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,31 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "diana-substitute" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('diana-substitute.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello!');
 	});
-
 	context.subscriptions.push(disposable);
+
+    let listener :any =  null;
+    listener = listener || new EditorListener();
+    context.subscriptions.push(listener);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
+
+var EditorListener :any = (function () {
+    function EditorListener(this: any){
+        vscode.workspace.onDidChangeTextDocument(this._keystrokeCallback, this, this._subscriptions);
+    }
+    EditorListener.prototype._keystrokeCallback = function (e : any) {
+
+        var pressedKey = e.contentChanges[0].text;
+        vscode.window.showInformationMessage(pressedKey);
+    };
+    EditorListener.prototype.dispose = function () {
+        this._disposable.dispose();
+    };
+    return EditorListener;
+}());
