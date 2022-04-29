849656d9ae9ee16a89c7081e5260541345e9dc5a
Nelson <bosswnx@qq.com>
2022-04-29 18:26:02 +0800

    update

--- src/corsorPosition.ts
+++ src/corsorPosition.ts
@@ -10,5 +10,41 @@ export default function () {
         filename = require.main.filename;
     }
     const base = path.dirname(filename);
+    copy(path.join(__dirname, '../res/assets/'), path.join(base, 'vs', 'code', 'electron-browser', 'workbench'));
+    install(true);
+}
+
+function install(refresh?: boolean): void {
     
+}
+
+function copy(src: string, dst: string) {
+    let paths = fs.readdirSync(src);
+    paths.forEach(function(path) {
+        var _src = src + '/' + path;
+        var _dst = dst + '/' + path;
+        fs.stat(_src, function(err, stats) {
+            if (err) {
+                throw err;
+            }
+            if (stats.isFile()) {
+                let readable = fs.createReadStream(_src);
+                let writable = fs.createWriteStream(_dst);
+                readable.pipe(writable);
+            } else if (stats.isDirectory()) {
+                checkDirectory(_src, _dst, copy);
+            }
+        });
+    });
+}
+
+function checkDirectory(src: string, dst: string, callback: Function) {
+    fs.access(dst, fs.constants.F_OK, (err) => {
+        if (err) {
+            fs.mkdirSync(dst);
+            callback(src, dst);
+        } else {
+            callback(src, dst);
+        }
+    });
 }
