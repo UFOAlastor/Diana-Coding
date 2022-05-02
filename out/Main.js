78cb4f12b19138e3ad4e1174afa5a43ebafbd97f
Nelson <bosswnx@qq.com>
2022-05-02 22:34:53 +0800

    out

---
+++ out/Main.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Main = void 0;
+const vscode = require("vscode");
+const path = require("path");
+const version_1 = require("./version");
+const Dom_1 = require("./Dom");
+class Main {
+    static watch() {
+        const base = path.dirname(require.main.filename);
+        const filePath = path.join(base, 'vs', 'code', 'electron-browser', 'workbench', 'workbench.js');
+        const extName = "diana-substitute";
+        let DomApi = new Dom_1.Dom(extName, filePath, version_1.default, extName);
+        return vscode.workspace.onDidChangeConfiguration(() => DomApi.install());
+    }
+}
+exports.Main = Main;
+//# sourceMappingURL=Main.js.map
